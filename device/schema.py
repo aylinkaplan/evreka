1a5836c63de46e81e65b12aaa42927b92e3c64e5
Aylin Kaplan <aylin@314radius.com>
2024-07-24 18:32:21 +0300

    Add a new schema : DeviceWithLastLocationType

--- device/schema.py
+++ device/schema.py
@@ -16,19 +16,25 @@ class LocationType(DjangoObjectType):
         fields = '__all__'
 
 
-class DeviceWithLastLocationType(graphene.ObjectType):
-    id = graphene.ID()
-    last_location_id = graphene.ID()
-    last_location_latitude = graphene.Float()
-    last_location_longitude = graphene.Float()
-    last_location_created_at = graphene.DateTime()
+class DeviceWithLastLocationType(DjangoObjectType):
+    last_location = graphene.Field(LocationType)
+
+    class Meta:
+        model = Device
+        fields = '__all__'
+
+    def resolve_last_location(self, info):
+        last_location = Location.objects.filter(
+            device=self
+        ).order_by('-created_at').first()
+        return last_location
 
 
 class Query(graphene.ObjectType):
     devices = graphene.List(DeviceType)
     locations = graphene.List(LocationType)
     device_location_history = graphene.List(LocationType, device_id=graphene.Int(required=True))
-    device_last_locations = graphene.List(DeviceWithLastLocationType)
+    devices_with_last_locations = graphene.List(DeviceWithLastLocationType)
 
     def resolve_devices(self, info, **kwargs):
         return Device.objects.all()
@@ -36,37 +42,30 @@ class Query(graphene.ObjectType):
     def resolve_locations(self, info, **kwargs):
         return Location.objects.all()
 
-    def resolve_device_location_history(root, info, device_id):
+    def resolve_device_location_history(self, info, device_id):
         try:
             device = Device.objects.get(pk=device_id)
         except Device.DoesNotExist:
             return None
         return Location.objects.filter(device=device).order_by('-created_at')
 
-    def resolve_device_last_locations(root, info):
-        latest_location_subquery = Location.objects.filter(
-            device=OuterRef('pk')
-        ).order_by('-created_at').values('id', 'latitude', 'longitude', 'created_at')[:1]
-
-        devices_with_last_location = Device.objects.annotate(
-            last_location_id=Subquery(latest_location_subquery.values('id')),
-            last_location_latitude=Subquery(latest_location_subquery.values('latitude')),
-            last_location_longitude=Subquery(latest_location_subquery.values('longitude')),
-            last_location_created_at=Subquery(latest_location_subquery.values('created_at')),
-        ).filter(last_location_id__isnull=False).values(
-            'id', 'last_location_id', 'last_location_latitude', 'last_location_longitude', 'last_location_created_at'
+    def resolve_devices_with_last_locations(self, info):
+        return Device.objects.annotate(
+            last_location_id=Subquery(
+                Location.objects.filter(device=OuterRef('pk')).order_by('-created_at').values('id')[:1]
+            ),
+            last_location_latitude=Subquery(
+                Location.objects.filter(device=OuterRef('pk')).order_by('-created_at').values('latitude')[:1]
+            ),
+            last_location_longitude=Subquery(
+                Location.objects.filter(device=OuterRef('pk')).order_by('-created_at').values('longitude')[:1]
+            ),
+            last_location_created_at=Subquery(
+                Location.objects.filter(device=OuterRef('pk')).order_by('-created_at').values('created_at')[:1]
+            )
+        ).filter(
+            last_location_id__isnull=False
         )
-
-        return [
-            {
-                'id': device['id'],
-                'last_location_id': device['last_location_id'],
-                'last_location_latitude': device['last_location_latitude'],
-                'last_location_longitude': device['last_location_longitude'],
-                'last_location_created_at': device['last_location_created_at'],
-            }
-            for device in devices_with_last_location
-        ]
 
 
 class CreateDevice(graphene.Mutation):
--- evreka/schema.py
+++ evreka/schema.py
@@ -4,12 +4,10 @@ import device.schema
 
 
 class Query(device.schema.Query, graphene.ObjectType):
-    # Combine the queries from different apps
     pass
 
 
 class Mutation(device.schema.Mutation, graphene.ObjectType):
-    # Combine the mutations from different apps
     pass
 
 
